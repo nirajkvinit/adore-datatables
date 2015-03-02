940e354e05683f7f45f02677a67a700b48d2c520
Adore ITS <mail@adorefoundation.com>
2015-03-02 18:54:06 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Adore Datatables
+Plugin URI: https://github.com/adoreits/adore-datatables
+Description: Adore Datatables (Wordpress and Datatables Integration Project)
+Version: 0.0.1
+Author: Niraj Kumar (Adore ITS)
+Author URI: http://www.adoreits.com/
+*/
+
+define("ADORE_DATATABLES", WP_PLUGIN_URL ."/adore-datatables");
